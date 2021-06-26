94284b6fd18f435748b344f863f37102beb56606
ishbosamiya <ishbosamiya@gmail.com>
2021-06-26 14:51:46 +0530

    dependencies and very basic setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use egui::Output;
+
+pub struct EguiBackend {
+    egui_ctx: egui::CtxRef,
+}
+
+impl EguiBackend {
+    pub fn new() -> Self {
+        return Self {
+            egui_ctx: Default::default(),
+        };
+    }
+
+    pub fn begin_frame(&mut self, raw_input: egui::RawInput) {
+        self.egui_ctx.begin_frame(raw_input);
+    }
+
+    pub fn end_frame(&self) -> Output {
+        let (output, shapes) = self.egui_ctx.end_frame();
+
+        let _meshes = self.egui_ctx.tessellate(shapes);
+
+        return output;
     }
 }
