04a19753dbe8a68cd08f31c60e4bf6547bd27bd3
ishbosamiya <ishbosamiya@gmail.com>
2021-11-06 15:15:16 +0530

    util: fix: doc: url warning

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,5 @@
 /// Get offset to struct member, similar to `offset_of` in C/C++
-/// From https://stackoverflow.com/questions/40310483/how-to-get-pointer-offset-in-bytes/40310851#40310851
+/// From <https://stackoverflow.com/questions/40310483/how-to-get-pointer-offset-in-bytes/40310851#40310851>
 #[macro_export]
 macro_rules! offset_of {
     ($ty:ty, $field:ident) => {
