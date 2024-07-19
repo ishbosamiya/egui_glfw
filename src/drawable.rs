72d8fcedb2e3abcc39aed8b4944d1cc9b617b8ec
ishbosamiya <ishbosamiya@gmail.com>
2024-07-19 18:56:43 +0530

    drawable: Drawable: remove draw_wireframe(): it isn't used

--- src/drawable.rs
+++ src/drawable.rs
@@ -1,6 +1,8 @@
+// TODO: need to eventually get rid of [`Drawable`] completely, it is
+// a terrible way to implement drawing/rendering. Having custom
+// methods that get called in a chain is just strictly better. This
+// trait achieves something similar but with extra steps that
+// introduces complexity.
 pub trait Drawable<ExtraData, Error> {
     fn draw(&self, extra_data: &mut ExtraData) -> Result<(), Error>;
-    fn draw_wireframe(&self, _extra_data: &mut ExtraData) -> Result<(), Error> {
-        todo!("error: draw_wireframe() not implemented but called");
-    }
 }
